302fcb92d911a54b633fd0c7800566c9173c3ed3
tjhelsel <terence.helsel@gmail.com>
2019-09-28 22:19:07 -0400

    added hello and mario

---
+++ hello.c
@@ -0,0 +1,10 @@
+//Sadly some functions/keywords will not work w/o cs50 available!
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string name = get_string("What is your name?\n");
+    printf("hello, %s\n", name);
+}
---
+++ mario.c
@@ -0,0 +1,34 @@
+//Sadly some functions/keywords will not work w/o cs50 available!
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Enter a number between 1 and 8:\n");
+    }
+    while (n < 1 || n > 8);
+    
+//     int len = n/2;
+    string row = "#";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n-i; j >= 0; j--)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            printf("%s",row);
+        }
+        printf("  ");
+        for (int j = 0; j <= i; j++)
+        {
+            printf("%s",row);
+        }
+        printf("\n");
+    }
+}
